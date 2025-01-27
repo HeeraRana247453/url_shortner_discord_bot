ea5689feefcf873a3aa81c9ba9196df849074014
Heera Rana <heerarana526@gmail.com>
2025-01-27 14:30:10 +0530

    install nodemon

--- connection.js
+++ connection.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 
-if(process.env.NODE_ENV !== "production"){
-    require("dotenv").config({path:"./config/.env"});
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config({ path: "./config/.env" });
 }
 
-
-async function connectToMongo(){
-    return mongoose.connect(process.env.DB_URL)
-            .then(()=>console.log(`MongoDB is connected!`))
-            .catch((err)=>console.log("Error: Not connected to Database",err))
+async function connectToMongo() {
+    try {
+        await mongoose.connect(process.env.DB_URL);
+        console.log("MongoDB is connected!");
+    } catch (err) {
+        console.error("Error: Not connected to Database", err.message);
+    }
 }
 
-module.exports = {connectToMongo};
+module.exports = { connectToMongo };
